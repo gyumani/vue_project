fe13935c0815c9b3d296bcf7ea12adcab7218eab
gyumani <atck1227@gmail.com>
2022-12-08 22:17:52 +0900

    [todo] first vue todo toy project initrialize
    gyumani

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooter from "./components/TodoFooter.vue";
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter
+  }
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f1f1f1;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
+}
+</style>
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    todoFooter
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoFooter"
+};
+</script>
+
+<style></style>
